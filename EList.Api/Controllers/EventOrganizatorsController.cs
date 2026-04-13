5c3c23b99dd1318e28ad371804add121e974d9c4
crash <crash240192@inbox.ru>
2026-04-14 01:29:42 +0300

    Добавил получение организаторов мероприятия

---
+++ EList.Api/Controllers/EventOrganizatorsController.cs
@@ -0,0 +1,64 @@
+﻿using EList.Common.CorrelationId;
+using EList.Common.Logger;
+using EList.Common.Models;
+using EList.DbDataProvider.Interfaces;
+using EList.Models.EventOrganizators;
+using EList.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System.Diagnostics;
+using TM.Schedule.API.Attributes;
+
+namespace EList.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    [LoggerHandlerWebApiFilter]
+    public class EventOrganizatorsController : ControllerBase
+    {
+        #region logger
+        private static readonly NLog.ILogger log = LogManager.GetCurrentClassLogger();
+        private static readonly ILoggerWrapper logger = new NLogLoggerWrapper(log);
+        private const string LOGGER_NAME = "EList.Api.Controllers.EventOrganizatorsController.";
+        #endregion
+
+        private readonly IEventOrganizatorsService _organizatorsService;
+        private readonly ICorrelationIdProvider _correlationIdProvider;
+        private readonly IDataConnectionProvider _connectionProvider;
+
+        public EventOrganizatorsController(ICorrelationIdProvider correlationIdProvider,
+            IDataConnectionProvider connectionProvider,
+            IEventOrganizatorsService organizatorsService
+            )
+        {
+            _organizatorsService = organizatorsService;
+            _correlationIdProvider = correlationIdProvider;
+            _connectionProvider = connectionProvider;
+        }
+
+        [HttpGet("getByEventId/{eventId}")]
+        public async Task<CommandResult<List<EventOrganizator>>> GetByEventIdAsync(Guid eventId)
+        {
+            var correlationId = _correlationIdProvider.Get();
+            var execTime = Stopwatch.StartNew();
+            var methodName = $"{LOGGER_NAME}{nameof(GetByEventIdAsync)}";
+
+            try
+            {
+                logger.Debug(correlationId, null, methodName, $"Method started", null);
+
+                var result = await _organizatorsService.GetByEventIdAsync(eventId);
+
+                logger.Debug(correlationId, null, methodName, $"Method finished", null, execTime.Elapsed);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                ExceptionLogger.LogException(logger, correlationId, methodName, "Method failed", execTime.Elapsed, ex);
+                throw;
+            }
+        }
+    }
+}
---
+++ EList.Services.Interfaces/IEventOrganizatorsService.cs
@@ -0,0 +1,10 @@
+﻿using EList.Common.Models;
+using EList.Models.EventOrganizators;
+
+namespace EList.Services.Interfaces
+{
+    public interface IEventOrganizatorsService
+    {
+        Task<CommandResult<List<EventOrganizator>>> GetByEventIdAsync(Guid eventId);
+    }
+}
